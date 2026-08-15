0ee878f15ab0361315f4aac5a0fc97e03666db91
Antipokemon <antipokemon@gmail.com>
2026-08-15 17:16:44 -0400

    test fix

--- Core/TargetScanner.cs
+++ Core/TargetScanner.cs
@@ -1,4 +1,3 @@
-using Dalamud.Game.ClientState.Objects.Enums;
 using Dalamud.Game.ClientState.Objects.Types;
 
 namespace HuntAutomator.Core;
@@ -8,10 +7,19 @@ internal static class TargetScanner
     public static IGameObject? Find(uint nameId, float radius)
     {
         var player = Service.ObjectTable.LocalPlayer;
-        if (player is null) return null;
+        if (player is null)
+            return null;
+
         return Service.ObjectTable
-            .Where(o => o.ObjectKind == ObjectKind.BattleNpc && o.IsTargetable && o.NameId == nameId)
-            .OrderBy(o => System.Numerics.Vector3.DistanceSquared(o.Position, player.Position))
-            .FirstOrDefault(o => System.Numerics.Vector3.Distance(o.Position, player.Position) <= radius);
+            .OfType<IBattleNpc>()
+            .Where(o => o.IsTargetable && o.NameId == nameId)
+            .OrderBy(o =>
+                System.Numerics.Vector3.DistanceSquared(
+                    o.Position,
+                    player.Position))
+            .FirstOrDefault(o =>
+                System.Numerics.Vector3.Distance(
+                    o.Position,
+                    player.Position) <= radius);
     }
 }
